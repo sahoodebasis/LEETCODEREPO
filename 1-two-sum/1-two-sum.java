600d3a802cffab6777c19ae3680074f0804507ef
Debasis Sahoo <77963202+sahoodebasis@users.noreply.github.com>
2022-04-03 15:15:11 +0530

    Time: 1 ms (99.76%), Space: 41.9 MB (99.71%) - LeetHub

--- 1-two-sum/1-two-sum.java
+++ 1-two-sum/1-two-sum.java
@@ -10,7 +10,7 @@ class Solution {
                }
                
                }
-               return new int[]{};
+               return null;
                }
                }
               
