6013db1ffd16619524c97616992d07a561b287cf
Debasis Sahoo <77963202+sahoodebasis@users.noreply.github.com>
2022-04-04 18:01:03 +0530

    Time: 15 ms (40.36%), Space: 45.1 MB (38.48%) - LeetHub

--- 9-palindrome-number/9-palindrome-number.java
+++ 9-palindrome-number/9-palindrome-number.java
@@ -1,20 +1,33 @@
 class Solution {
     public boolean isPalindrome(int x) {
         
-        if(x<0){
-            return false;
-        }
-        int n=x;
-        int sum=0;
-        while(n>0){
-            int r=n%10;
-            sum=10*sum+r;
-            n=n/10;
-        }
-        if(sum==x){
-            return true;
-        }
+//         if(x<0){
+//             return false;
+//         }
+//         int n=x;
+//         int sum=0;
+//         while(n>0){
+//             int r=n%10;
+//             sum=10*sum+r;
+//             n=n/10;
+//         }
+//         if(sum==x){
+//             return true;
+//         }
         
-        return false;
+//         return false;
+        String str=Integer.toString(x);
+        int i=0;
+        int j=str.length()-1;
+        while(i<=j){
+            if(str.charAt(i)!=str.charAt(j)){
+                return false;
+            }
+            else{
+                i++;
+                j--;
+            }
+        }
+        return true;
     }
 }
