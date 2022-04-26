c1236eadd623382ab0b741087f28092b3bdfe038
Debasis Sahoo <77963202+sahoodebasis@users.noreply.github.com>
2022-04-26 23:30:29 +0530

    Time: 11 ms (35.20%), Space: 42.6 MB (84.23%) - LeetHub

--- 13-roman-to-integer/13-roman-to-integer.java
+++ 13-roman-to-integer/13-roman-to-integer.java
@@ -1,29 +1,38 @@
 class Solution {
     public int romanToInt(String s) {
          
-    int n = s.length();
-    Map<Character, Integer> map = new HashMap<>();
-    map.put('I', 1);
-    map.put('V', 5);
-    map.put('X', 10);
-    map.put('L', 50);
-    map.put('C', 100);
-    map.put('D', 500);
-    map.put('M', 1000);
-    
-    int sum = 0;
-    
-    for (int i = n-1; i >= 0; i--) {
-        char ch = s.charAt(i);
+        Map<String,Integer> map1=new HashMap<>();
+        map1.put("I",1);
+        map1.put("IV",4);
+        map1.put("V",5);
+        map1.put("IX",9);
+        map1.put("X",10);
+        map1.put("XL",40);
+        map1.put("L",50);
+        map1.put("XC",90);
+        map1.put("C",100);
+        map1.put("CD",400);
+        map1.put("D",500);
+        map1.put("CM",900);
+        map1.put("M",1000);
+        s=s+" ";
         
-        if (i > 0 && map.get(ch) > map.get(s.charAt(i-1))) {
-            int val = map.get(ch) - map.get(s.charAt(i-1));
-            sum += val;
-            i--;
+        int sum=0;
+        for(int i=0;i<s.length()-1;i++){
+            char char1=s.charAt(i);
+            char char2=s.charAt(i+1);
+            String str=Character.toString(char1)+Character.toString(char2);
+            if(map1.containsKey(str)){
+                sum+=map1.get(str);
+                i++;
+            }
+            else{
+                // str=char1+"";
+                str=Character.toString(char1);
+                sum+=map1.get(str);
+            }
+            
         }
-        else sum += map.get(ch);
-    }
-    
-    return sum;
+        return sum;
     }
 }
