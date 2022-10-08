3a6236de00cf5480c211f479aa2a65b5bbe2cdf5
Debasis Sahoo <77963202+sahoodebasis@users.noreply.github.com>
2022-10-08 15:59:05 +0530

    Time: 0 ms (100.00%), Space: 40.2 MB (80.88%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.java
+++ 70-climbing-stairs/70-climbing-stairs.java
@@ -1,20 +1,31 @@
 class Solution {
-    Map<Integer,Integer> hashMap=new HashMap<>();
+    // Map<Integer,Integer> hashMap=new HashMap<>();
     
     public int climbStairs(int n) {
-        hashMap.put(1,1);
-        hashMap.put(2,2);
-        int answer=climbCount(n);
-        return answer;
-        
-    }
-    public int climbCount(int n){
-        if(hashMap.containsKey(n)){
-            return hashMap.get(n);
+        // hashMap.put(1,1);
+        // hashMap.put(2,2);
+        // int answer=climbCount(n);
+        // return answer;
+        if(n<=2){
+            return n;
         }
-        else{
-            hashMap.put(n,climbCount(n-1)+climbCount(n-2));
-            return hashMap.get(n);
+        int firstVal=1;
+        int secondVal=2;
+        int thirdVal=0;
+        for(int i=3;i<=n;i++){
+            thirdVal=firstVal+secondVal;
+            firstVal=secondVal;
+            secondVal=thirdVal;
         }
+        return thirdVal;
     }
+    // public int climbCount(int n){
+    //     if(hashMap.containsKey(n)){
+    //         return hashMap.get(n);
+    //     }
+    //     else{
+    //         hashMap.put(n,climbCount(n-1)+climbCount(n-2));
+    //         return hashMap.get(n);
+    //     }
+    // }
 }
