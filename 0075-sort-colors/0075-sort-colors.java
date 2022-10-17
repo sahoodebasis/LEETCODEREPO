792f23e399994ff165b537a471c5fe18f1fa5316
Debasis Sahoo <77963202+sahoodebasis@users.noreply.github.com>
2022-10-18 00:37:11 +0530

    Time: 1 ms (53.76%), Space: 42.5 MB (44.37%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.java
+++ 0075-sort-colors/0075-sort-colors.java
@@ -1,6 +1,6 @@
 class Solution {
     public void sortColors(int[] nums) {
-        int zero=0, one=0,two=0;
+        int zero=0, one=0;
         for(int i=0;i<nums.length;i++){
             if(nums[i]==0){
                 zero++;
@@ -8,9 +8,7 @@ class Solution {
             else if(nums[i]==1){
                 one++;
             }
-            else{
-                two++;
-            }
+            
         }
         for(int i=0;i<nums.length;i++){
             if(i>=0 && i< zero){
