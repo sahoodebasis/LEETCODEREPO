d316b36e998cf63c1057e5cc49609526277c4164
Debasis Sahoo <77963202+sahoodebasis@users.noreply.github.com>
2022-11-11 02:34:15 +0530

    Time: 2 ms (93.39%), Space: 45.6 MB (96.01%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,18 +1,20 @@
 class Solution {
     public int majorityElement(int[] nums) {
-        int count=0, majorElement=nums[0];
-        for(int i: nums){
-            if(count==0){
-                majorElement=i;
-                count++;
-            }
-            else if(i==majorElement){
-                count++;
-            }
-            else{
-                count--;
-            }
-        }
-        return majorElement;
+        // int count=0, majorElement=nums[0];
+        // for(int i: nums){
+        //     if(count==0){
+        //         majorElement=i;
+        //         count++;
+        //     }
+        //     else if(i==majorElement){
+        //         count++;
+        //     }
+        //     else{
+        //         count--;
+        //     }
+        // }
+        // return majorElement;
+         Arrays.sort(nums);
+     return nums[nums.length/2];
     }
 }
