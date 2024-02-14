0c8de5f1ebdbb7042a2ebfd6ab983b65f3647642
Debasis Sahoo <77963202+sahoodebasis@users.noreply.github.com>
2024-02-15 02:18:26 +0530

    Time: 0 ms (100.00%), Space: 43 MB (57.29%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.java
+++ 0031-next-permutation/0031-next-permutation.java
@@ -1,33 +1,34 @@
 class Solution {
     public void nextPermutation(int[] nums) {
+        
         int i=nums.length-2, j=nums.length-1;
         while(i>=0 && nums[i]>=nums[i+1]){
             i--;
         }
         if(i>=0){
-            while(j>=0 && nums[j]<=nums[i]){
+            while(j>=i && nums[i] >=nums[j]){
                 j--;
             }
-            swap(nums, i ,j);
+            swap(nums,i,j);
         }
-        reverse(nums, i+1);
+        reverse(nums,i+1);
     }
     
-    
-    public void swap(int[] nums,int i,int j){
+    public void swap(int nums[],int i ,int j ){
         int temp=0;
         temp=nums[i];
         nums[i]=nums[j];
         nums[j]=temp;
     }
     
-    
-    public void reverse(int [] nums,int i){
-        int j=nums.length-1;
-        while(i< j){
-            swap(nums,i,j);
-            i++;
-            j--;
-        }
-    }
+     public void reverse(int nums[],int i){
+
+         int j=nums.length-1;
+         while(i<=j){
+         swap(nums,i,j);
+         i++;
+         j--;
+         }
+         
+     }
 }
