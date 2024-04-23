fb47604f7fc2a3d632ef839395a29c8e14df84e7
Debasis Sahoo <77963202+sahoodebasis@users.noreply.github.com>
2024-04-24 01:51:30 +0530

    Time: 1 ms (85.13%), Space: 41.9 MB (52.32%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.java
+++ 0118-pascals-triangle/0118-pascals-triangle.java
@@ -1,23 +1,19 @@
 class Solution {
     public List<List<Integer>> generate(int numRows) {
-
-        List<List<Integer>> solList= new ArrayList<>();
         
+        List<List<Integer>> list = new ArrayList<>();
         for(int i=1;i<=numRows;i++){
-            List<Integer> tempList= new ArrayList<>();
-            for(int j=1;j<=i;j++){
-                
-                if(j==1 || j==i){
-                    tempList.add(1);
+            List<Integer> subList=new ArrayList<>();
+            for(int j=0;j<i;j++) {
+                if(j==0 || j==i-1){
+                    subList.add(1);
                 }
-                else{
-                    tempList.add(solList.get(i-2).get(j-1)+solList.get(i-2).get(j-2));
+                else {
+                    subList.add(list.get(i-2).get(j-1)+ list.get(i-2).get(j));
                 }
             }
-            solList.add(tempList);
+            list.add(subList);
         }
-        
-        return solList;
-        
+        return list;
     }
 }
