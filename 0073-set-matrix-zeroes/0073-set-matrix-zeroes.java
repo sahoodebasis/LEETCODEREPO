aa154d4e799015344e34c2a60cb1c626cd71dd8a
Debasis Sahoo <77963202+sahoodebasis@users.noreply.github.com>
2024-06-05 11:06:48 +0530

    Time: 0 ms (100.00%), Space: 45.7 MB (11.56%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -1,26 +1,62 @@
 class Solution {
     public void setZeroes(int[][] matrix) {
+        
         int m=matrix.length;
         int n=matrix[0].length;
-        Set<Integer> row=new HashSet<>();
-        Set<Integer> col=new HashSet<>();
+        
+        boolean firstRowZero=false;
+        boolean firstColZero=false;
+        
+        for(int j=0;j<n;j++){
+            if(matrix[0][j]==0){
+                firstRowZero=true;
+                break;
+                
+            }
+        }
+        
         for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
+            if(matrix[i][0]==0){
+                firstColZero=true;
+            }
+            
+            for(int j=1;j<n;j++){
                 if(matrix[i][j]==0){
-                    row.add(i);
-                    col.add(j);
+                    matrix[i][0]=0;
+                    matrix[0][j]=0;
                 }
             }
-        }
-        for(Integer z:row){
-            for(int j=0;j<n;j++){
-                matrix[z][j]=0;
+        
+                
+            }
+        
+        for(int i=1;i<m;i++){
+            if(matrix[i][0]==0){
+                 for(int j=1;j<n;j++){
+                    matrix[i][j]=0;
+                }
+            }
+            }
+        
+        for(int j=1;j<n;j++){
+            if(matrix[0][j]==0){
+                 for(int i=1;i<m;i++){
+                    matrix[i][j]=0;
+                }
+            }
+            }
+        
+        if(firstRowZero) {
+            for( int j =0;j<n;j++) {
+                matrix[0][j]=0;
             }
         }
-        for(Integer k:col){
-            for(int i=0;i<m;i++){
-                matrix[i][k]=0;
+        
+        
+        if(firstColZero) {
+            for( int i =0;i<m;i++) {
+                matrix[i][0]=0;
             }
         }
-    }
-}
+        }
+        }
