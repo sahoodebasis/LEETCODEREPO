947116cf68bb3deead7c4d5dad5075638430098d
Debasis Sahoo <77963202+sahoodebasis@users.noreply.github.com>
2024-06-07 15:32:16 +0530

    Time: 1 ms (99.49%), Space: 56.6 MB (84.59%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.java
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -5,16 +5,8 @@ class Solution {
         int currentSum=nums[0];
         
         for(int i=1;i<nums.length;i++) {
-            if(currentSum+nums[i] >nums[i]){
-                currentSum=currentSum+nums[i];
-            }
-            else{
-                currentSum=nums[i];
-            }
-            
-            if(currentSum>largestSum){
-                largestSum=currentSum;
-            }
+            currentSum=Math.max(currentSum+nums[i],nums[i]);
+            largestSum=Math.max(currentSum,largestSum);
         }
         
         return largestSum;
