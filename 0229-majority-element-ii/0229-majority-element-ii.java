be78882633f8921d45da7ea7257f7d057d5e196e
Debasis Sahoo <77963202+sahoodebasis@users.noreply.github.com>
2024-06-16 11:05:20 +0530

    Time: 4 ms (55.46%), Space: 48.2 MB (23.56%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.java
+++ 0229-majority-element-ii/0229-majority-element-ii.java
@@ -1,45 +1,52 @@
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
-        int count1=0,count2=0,majority1=nums[0],majority2=nums[0],n=nums.length;
-        List<Integer> list=new ArrayList<>();
-        for(int x:nums){
-           if(majority1==x){
+        
+        List<Integer> list = new ArrayList<>();
+        int count1=0,count2=0,majorElement1=nums[0],majorElement2=nums[0], n=nums.length;
+        
+        for(int i=0;i<n;i++){
+            if(majorElement1==nums[i]){
                 count1++;
             }
-            else if(majority2==x){
-                count2++;
-            }
-            else if(count1==0){
-                majority1=x;
-                count1++;
-            }
-            else if(count2==0){
-                majority2=x;
+            else if(majorElement2==nums[i]){
                 count2++;
             }
             
-            else{
+           else if(count1==0){
+                count1++;
+                majorElement1=nums[i];
+            }
+            else if (count2==0){
+                count2++;
+                majorElement2=nums[i];
+            }
+             else {
                 count1--;
                 count2--;
             }
+           
         }
-        
+      //here we found out the major elements. Now we need to count each element count and check whether it is greater than n/3.
         count1=0;
         count2=0;
-        for(int x: nums){
-            if(x==majority1){
+        
+        for(Integer x: nums){
+            if(x==majorElement1){
                 count1++;
             }
-            else if(x==majority2){
+           else if(x==majorElement2){
                 count2++;
             }
         }
+        
         if(count1>n/3){
-            list.add(majority1);
+            list.add(majorElement1);
         }
         if(count2>n/3){
-            list.add(majority2);
+            list.add(majorElement2);
         }
+        
         return list;
+        
     }
 }
