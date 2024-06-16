f23676653aaacaf76493add19dc65ceab9326a35
Debasis Sahoo <77963202+sahoodebasis@users.noreply.github.com>
2024-06-16 19:52:56 +0530

    Time: 5 ms (86.52%), Space: 44.4 MB (69.13%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
@@ -4,25 +4,18 @@ class Solution {
         int maxLen=0,curLen=0;
         for(int i=0;i<s.length();i++){
             String currentChar = String.valueOf(s.charAt(i));
-            System.out.println("currentChar "+currentChar);
             if(str.indexOf(currentChar)==-1){
-                System.out.println("str.indexOf(currentChar) "+str.indexOf(currentChar));
                 str.append(currentChar);
                 curLen++;
-                System.out.println("curLen "+curLen);
             }
             else{
 
                 str.delete(0,str.indexOf(currentChar)+1);
-                 System.out.println("str after delete "+str);
                 str.append(currentChar);
-                 System.out.println("str+ "+str);
                 curLen=str.length();
-                 System.out.println("curLen+ "+curLen);
             }
             
             maxLen=Math.max(maxLen,curLen);
-             System.out.println("maxLen "+maxLen);
         }
         return maxLen;
     }
