e2fd85d81126ac09985972754ee20f473df46c65
Debasis Sahoo <77963202+sahoodebasis@users.noreply.github.com>
2024-06-23 15:01:01 +0530

    Time: 0 ms (100.00%), Space: 44.6 MB (15.13%) - LeetHub

---
+++ 0046-permutations/0046-permutations.java
@@ -0,0 +1,30 @@
+class Solution {
+    List<List<Integer>> list=  new ArrayList<>();
+    public List<List<Integer>> permute(int[] nums) {
+        int n=nums.length;
+        int arr[]=new int[n];
+        Arrays.fill(arr,11);
+        findPermutation(nums,arr,0);
+        return list;
+    }
+    
+    public void findPermutation(int [] nums, int [] arr,int index) {
+        if(index==nums.length){
+            List<Integer> subList= new ArrayList<>();
+            for(int i=0;i<arr.length;i++){
+                subList.add(arr[i]);
+            }
+            list.add(subList);
+            return;
+        }
+        
+        for(int i=0;i<nums.length;i++){
+            if(arr[i]==11){
+                arr[i]=nums[index];
+                findPermutation(nums,arr,index+1);
+                arr[i]=11;
+            }
+        }
+    }
+}
+//https://www.youtube.com/watch?v=XUQ0iZ94CGg&ab_channel=Let%27sCodeTogether
